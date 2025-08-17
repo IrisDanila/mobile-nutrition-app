3ad96c42516fed4aab9663106f2706319a38a25b
Iris Danila <daniela.danila2003@gmail.com>
2025-08-17 14:34:59 +0300

    hided the top navbar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeGradientProvider, useGradient } from './src/providers/ThemeGradientProvider';
 import { GamificationEventsProvider } from './src/providers/GamificationEvents';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -15,11 +15,16 @@ const GradientShell: React.FC<{ children: React.ReactNode }> = ({ children }) =>
 };
 
 export default function App() {
+  // Previously attempted to hide Android nav bar via expo-navigation-bar.
+  // Removed to avoid runtime error when the package isn't installed.
+  // To re-enable, install with: npx expo install expo-navigation-bar
+  // then restore dynamic import code.
+  useEffect(()=>{}, []);
   return (
     <ThemeGradientProvider>
       <GamificationEventsProvider>
         <GradientShell>
-          <StatusBar barStyle='light-content' />
+          <StatusBar hidden translucent backgroundColor='transparent' barStyle='light-content' />
           <AppNavigator />
         </GradientShell>
       </GamificationEventsProvider>
